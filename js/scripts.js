eaf5e8205cca33c0d823a40e07dbd775be2b8281
Andrew Peterson <peterson8000@yahoo.com>
2018-01-09 16:22:06 -0800

    add files

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,29 @@
 $(document).ready(function() {
 
 
+  $("#term1").click(function() {
+    $("#term1Hidden").toggle();
+  });
+
+  $("#term2").click(function() {
+    $("#term2Hidden").toggle();
+  });
+
+  $("#term3").click(function() {
+    $("#term3Hidden").toggle();
+  });
+
+  $("#term4").click(function() {
+    $("#term4Hidden").toggle();
+  });
+
+  $("#term5").click(function() {
+    $("#term5Hidden").toggle();
+  });
+
+
+
+
 
 
 
